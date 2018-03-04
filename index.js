0abda5d24ed9e98a2693206e92d774a2d999d47a
David Feltell <dave@feltell.net>
2018-03-04 16:50:59 +0000

    Fix to work for updated canterbury.co.uk site
    * They completely redesigned the site.

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const Job = require('cron').CronJob;
 
 
 const MAX_TIME = 60000;
+const MAX_MONTHS = 4;
+
+const oneDay = 3600*24*1000;
 
 let logger = require('logger').createLogger(),
 	lastUpdated = 0,
@@ -21,7 +24,7 @@ const scrapeEvents = async ()=> {
 	if (isUpdating)
 		return;
 	isUpdating = true;
-	
+
 	try {
 		const startTime = Date.now();
 		const browser = await puppeteer.launch({
@@ -30,58 +33,62 @@ const scrapeEvents = async ()=> {
 				'--disable-setuid-sandbox'
 			]
 		});
-	
+
 		const page = await browser.newPage();
-	
+
 		page.on('console', msg => logger.info('PAGE LOG:', msg.text));
-	
-		await page.goto(
-			'http://www.canterbury.co.uk/events/thedms.aspx?dms=12&msg=', 
-			{waitUntil: 'networkidle0'}
-		);
-	
+
+		const today = new Date();
+		const startMonth = new Date(today.getFullYear(), today.getMonth());
+		const endMonth = new Date(today.getFullYear(), today.getMonth() + MAX_MONTHS);
+		const startMonthStr = moment(startMonth).format("DD-MM-YYYY");
+		const endMonthStr = moment(endMonth).format("DD-MM-YYYY");
+
+		let url = `https://www.canterbury.co.uk/events/start/${startMonthStr}/end/${endMonthStr}`;
+
+		logger.info("Scraping from " + url);
+
+		await page.goto(url, {waitUntil: "domcontentloaded"});
+
 		let calendarEvents = [],
 			numPages = 0;
-	
+
 		logger.info("Loaded first page, awaiting render");
-		await page.$("a.pagenextbrowsedata12")
-	
+		await page.$("a.pagination__link");
+
 		// Loop pages until we've scraped them all.
 		while (true) {
 			numPages++;
-			logger.info("On page " + numPages + ". Awaiting loading spinner");
-			// Ensure loading spinner is gone.
-			await page.waitForSelector("#loadinganimation", {hidden: true});
-			logger.info("Scraping events");
+			logger.info("On page " + numPages);
 			// Get the next lump of events.
 			calendarEvents.push(...await getEventsForPage(page));
-	
+
 			// Limit running time.
 			if (Date.now() - startTime > MAX_TIME) {
-				break
-			}
-	
-			if (!await page.$("a.pagenextbrowsedata12"))
 				break;
-	
+			}
+
+			if (!await page.$("a.pagination__link"))
+				break;
+
 			logger.info("Clicking for next page");
 			// Go to next page
-			await page.click("a.pagenextbrowsedata12");
-			logger.info("Awaiting navigation");
-			// Wait for navigation to have finished.
-			await page.waitForNavigation({waitUntil: "networkidle0"});
+			await Promise.all([
+				page.waitForNavigation({waitUntil: "domcontentloaded"}),
+				page.click("li.pagination__item--next > a.pagination__link")
+			]);
 		}
-	
+
 		logger.info("Scraped " + numPages + " pages in " + (Date.now() - startTime)/1000 + " secs");
-	
+
 		browser.close();
-	
+
 		fs.writeFile(
 			"/app/events.json", JSON.stringify(calendarEvents),
 			()=>logger.info("Written to events.json")
 		);
-		
-		lastUpdated = Date.now()
+
+		lastUpdated = Date.now();
 	} finally {
 		isUpdating = false;
 	}
@@ -89,20 +96,23 @@ const scrapeEvents = async ()=> {
 
 const getEventsForPage = async (page)=> {
 	let events = await page.evaluate(()=> {
-		let events = $(".thedmsBrowseRow").find(".regularScreenOnly").toArray().map((row)=> {
+		let field = ($row, name)=>{
+			$el = $row.find(`strong:contains('${name}:')`).parent();
+			return $el.contents().not($el.children()).text().trim();
+		};
+		let events = $("article.listing").toArray().map((row)=> {
 			let $row = $(row),
-				$title = $row.find(".thedmsBrowseH2Background"),
-				title = $title.not($title.children()).text().trim(),
-				address = $row.find(".thedmsAddress").text().trim(),
-				phone = $row.find(".thedmsPhone").text().trim(),
-				url = $row.find(".thedmsWebsite").text().trim(),
-				$date = $row.find(".thedmsEventDate"),
-				$description = $row.find(".thedmsDescription"),
-				description = $description.text().trim(),
-				dates = $date.find("strong  > a").text().trim(),
-				times = $date.find("ul > li").text().trim();
+				title = $row.find("h2.listing__heading").text().trim(),
+				address = field($row, "Location")
+					// Trim whitespace and commas, then join to single line with comma separator.
+					.split("\n").map(d=>d.replace(/^\s+|[\s,]+$/gm, "")).join(", ");
+				url = $row.find("a.listing__link").attr("href"),
+				dates = field($row, "Date"),
+				times = field($row, "Time"),
+				description = $row.find("p.listing__summary").text().trim(),
+				cost = field($row, "Cost");
 			return {
-				title, address, phone, url, dates, times, description
+				title, address, url, dates, times, description, cost
 			};
 		});
 
@@ -114,84 +124,40 @@ const getEventsForPage = async (page)=> {
 
 	for (event of events) {
 		let id,
-			currDate,
-			endDate,
-			dateMatch,
+			url = event.url,
 			title = _.compact([
-				event.title, event.description, event.address, event.phone
+				event.title, event.description, event.address, event.cost
 			]).join("\n\n"),
-			url = "http://" + event.url,
-			distinctTimes = event.times.split(",").map((time)=>time.trim()).map(
-				(time)=> time.split("to").map((time)=>time.trim())
-			);
-
-		if (dateMatch = datePatterns.singleDate.exec(event.dates)) {
-			currDate = moment(dateMatch.slice(1).join(" "), "D MMM YYYY");
-			endDate = moment(currDate);
-
-		} else if (dateMatch = datePatterns.singleMonth.exec(event.dates)) {
-			currDate = moment([dateMatch[1], ...dateMatch.slice(3, 5)].join(" "), "D MMM YYYY");
-			endDate = moment(dateMatch.slice(2, 5).join(" "), "D MMM YYYY");
-
-		} else if (dateMatch = datePatterns.multiMonth.exec(event.dates)) {
-			currDate = moment(
-				[...dateMatch.slice(1,3), dateMatch[5]].join(" "), "D MMM YYYY"
-			);
-			endDate = moment(dateMatch.slice(2, 5).join(" "), "D MMM YYYY");
-
-		} else if (dateMatch = datePatterns.multiYear.exec(event.dates)) {
-			currDate = moment(dateMatch.slice(1, 4).join(" "), "D MMM YYYY");
-			endDate = moment(dateMatch.slice(4, 7).join(" "), "D MMM YYYY");
-		} else {
-			throw new Error("'" + event.dates + "' did not match any date formats")
-		}
+			dates = event.dates.split("-").map(d=>d.trim()),
+			times = event.times.split("-").map(t=>t.trim()),
+			startDate = moment(dates[0], "Do MMMM YYYY"),
+			endDate = dates.lenth > 1 ? moment(dates[1], "Do MMMM YYYY") : startDate,
+			startTime = moment.duration(times[0]),
+			endTime = times.length > 1 ? moment.duration(times[1]) : startTime,
+			currDate = startDate.clone();
 
 		id = md5(title + url);
 
 		do {
-			if (!distinctTimes[0][0])  {
-				let start = moment(currDate),
-					allDay = true;
-				calendarEvents.push({id, start, title, url, allDay})
+			if (startTime.asMilliseconds() === 0)  {
+				let allDay = true,
+					start = currDate;
+				calendarEvents.push({id, start, title, url, allDay});
 
 			} else {
-				for (let times of distinctTimes) {
-					let start = moment(currDate).add(moment.duration(times[0])),
-						calendarEvent = {id, start, title, url};
-					if (times.length > 1)
-						calendarEvent.end = moment(currDate).add(moment.duration(times[1]));
-					calendarEvents.push(calendarEvent)
-				}
+				let start = moment(currDate).add(startTime),
+					calendarEvent = {id, start, title, url};
+				if (endTime - startTime > 0)
+					calendarEvent.end = moment(currDate).add(endTime);
+				calendarEvents.push(calendarEvent);
 			}
 
 			currDate.add(1, "days");
-		} while (currDate.isSameOrBefore(endDate))
+		} while (currDate.isSameOrBefore(endDate));
 	};
 
 	return calendarEvents;
 };
-
-//Date patterns to match.
-const datePatterns = {
-	singleDate: new RegExp(
-		"^[A-Z][a-z][a-z] ([0-9][0-9]?) ([A-Z][a-z][a-z]) ([0-9][0-9][0-9][0-9])$"
-	),
-	singleMonth: new RegExp(
-		"^[A-Z][a-z][a-z] ([0-9][0-9]?) - " +
-		"[A-Z][a-z][a-z] ([0-9][0-9]?) ([A-Z][a-z][a-z]) ([0-9][0-9][0-9][0-9])$"
-	),
-	multiMonth: new RegExp(
-		"^[A-Z][a-z][a-z] ([0-9][0-9]?) ([A-Z][a-z][a-z]) - " +
-		"[A-Z][a-z][a-z] ([0-9][0-9]?) ([A-Z][a-z][a-z]) ([0-9][0-9][0-9][0-9])$"
-	),
-	multiYear: new RegExp(
-		"^[A-Z][a-z][a-z] ([0-9][0-9]?) ([A-Z][a-z][a-z]) ([0-9][0-9][0-9][0-9]) - " +
-		"[A-Z][a-z][a-z] ([0-9][0-9]?) ([A-Z][a-z][a-z]) ([0-9][0-9][0-9][0-9])$"
-	)
-
-};
-
-const oneDay = 3600*24*1000;
 
 // Check every 5 mins if we need to update (have to do this way in case instance is frozen).
 const checkUpdate = new Job("00 */5 * * * *", ()=>{
@@ -199,7 +165,17 @@ const checkUpdate = new Job("00 */5 * * * *", ()=>{
 		logger.info("Events list is out of date, updating");
 		scrapeEvents();
 	}
-}, null, true, "Europe/London", null, true);
+}, null, false, "Europe/London");
+
+// Give time for Docker to get it's network up and running.
+setTimeout(()=>{
+	logger.info("Performing initial scrape");
+	try {
+		scrapeEvents();
+	} finally {
+		checkUpdate.start();
+	}
+}, 3000);
 
 // Web app to respond to requests.
 const app = express();
@@ -219,6 +195,4 @@ const server = app.listen(3000, () => logger.info("Canterbury Events app listeni
 		server.close(()=> logger.info("HTTP server stopped."));
 	});
 });
-
-
 
